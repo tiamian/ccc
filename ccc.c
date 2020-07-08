0517c6efe14fa64b7dc83afdb61c0f8c22c3e348
ian <ian1995615@gmail.com>
2020-07-08 22:27:39 +0800

    add and sub function

--- ccc.c
+++ ccc.c
@@ -2,15 +2,37 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
+    if (argc != 2) {
     fprintf(stderr, "引數數量錯誤\n");
     return 1;
-  }
+}
+char *p = argv[1];
 
-  printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
-  printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
-  printf("  ret\n");
-  return 0;
+printf(".intel_syntax noprefix\n");
+printf(".global main\n");
+printf("main:\n");
+printf("  mov rax, %d\n", strtol(p, &p, 10));
+
+while (*p)
+{
+    if (*p == '+')
+    {
+        p++;
+        printf("  add rax, %d\n", strtol(p, &p, 10));
+        continue;
+    }
+    
+    if (*p == '-')
+    {
+        p++;
+        printf("  sub rax, %d\n", strtol(p, &p, 10));
+        continue;
+    }
+    
+    fprintf(stderr, "預料之外的文字: '%c'\n", *p);
+    return 1;
+}
+
+printf("  ret\n");
+return 0;
 }
--- test.sh
+++ test.sh
@@ -16,7 +16,7 @@ try() {
   fi
 }
 
-try 0 0
+try 5 1+4
 try 42 42
 
 echo OK
